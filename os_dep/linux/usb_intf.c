db7bf6b8ff255ae6d6376ad980d96df923400344
Randy C. Will <randall.will@gmail.com>
2018-06-20 19:41:37 -0700

    Add EW-7822ULC device IDs

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -140,6 +140,7 @@ static void rtw_dev_shutdown(struct device *dev)
 
 
 #define USB_VENDER_ID_REALTEK		0x0BDA
+#define USB_VENDER_ID_EDIMAX		0x7392  //EDX
 
 
 /* DID_USB_v916_20130116 */
@@ -234,6 +235,8 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	/*=== Realtek demoboard ===*/
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0xB82C, 0xff, 0xff, 0xff), .driver_info = RTL8822B}, /* Default ID for USB multi-function */
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0xB812, 0xff, 0xff, 0xff), .driver_info = RTL8822B}, /* Default ID for USB Single-function, WiFi only */
+	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_EDIMAX, 0xB822, 0xff, 0xff, 0xff), .driver_info = RTL8822B}, /* EW-7822ULC */
+	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_EDIMAX, 0xC822, 0xff, 0xff, 0xff), .driver_info = RTL8822B}, //EDX
 	{USB_DEVICE(0x2001, 0x331c), .driver_info = RTL8822B}, /* D-Link - Cameo */
 #endif /* CONFIG_RTL8822B */
 
